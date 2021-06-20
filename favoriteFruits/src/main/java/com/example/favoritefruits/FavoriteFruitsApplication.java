7f871702581b1323fb91d6a7d23182b76fe1a6bd
HyunwookPark <park.hyunwook@planaria.co.jp>
2021-06-20 19:14:56 +0900

    プロジェクト新規作成

---
+++ favoriteFruits/src/main/java/com/example/favoritefruits/FavoriteFruitsApplication.java
@@ -0,0 +1,13 @@
+package com.example.favoritefruits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavoriteFruitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FavoriteFruitsApplication.class, args);
+	}
+
+}
