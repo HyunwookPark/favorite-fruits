77b3c5405414b00f0e84e9618a9aa3082519b5a3
HyunwookPark <park.hyunwook@planaria.co.jp>
2021-09-26 13:52:22 +0900

    MainにServletInitializer指定

--- src/main/java/com/example/favoritefruits/FavoriteFruitsApplication.java
+++ src/main/java/com/example/favoritefruits/FavoriteFruitsApplication.java
@@ -2,9 +2,10 @@ package com.example.favoritefruits;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FavoriteFruitsApplication {
+public class FavoriteFruitsApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FavoriteFruitsApplication.class, args);
